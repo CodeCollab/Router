e7e0f377d14547c636b2f067861f3a8e1c029734
PeeHaa <info@pieterhordijk.com>
2015-08-26 20:30:54 +0200

    Implemented router

---
+++ src/Router.php
@@ -0,0 +1,142 @@
+<?php declare(strict_types=1);
+/**
+ * The router class
+ *
+ * This router defines routes based on nikic's FastRoute project
+ *
+ * PHP version 7.0
+ *
+ * @category   CodeCollab
+ * @package    Router
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ * @copyright  Copyright (c) 2015 Pieter Hordijk <https://github.com/PeeHaa>
+ * @license    See the LICENSE file
+ * @version    1.0.0
+ */
+namespace CodeCollab\Router;
+
+use FastRoute\RouteCollector;
+use FastRoute\Dispatcher;
+
+/**
+ * The router class
+ *
+ * @category   CodeCollab
+ * @package    Router
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ */
+class Router
+{
+    /**
+     * @param \FastRoute\RouteCollector Collector for routes defined in the system
+     */
+    private $routeCollector;
+
+    /**
+     * @param callable Factory for the dispatcher
+     */
+    private $dispatcherFactory;
+
+    /**
+     * @param string Filename of the cache file
+     */
+    private $cacheFile;
+
+    /**
+     * @param bool Whether to invalidate and reload the cache
+     */
+    private $forceReload;
+
+    /**
+     * Creates instance
+     *
+     * @param \FastRoute\RouteCollector $routeCollector    Collector for routes defined in the system
+     * @param callable                  $dispatcherFactory Factory for the dispatcher
+     * @param string                    $cacheFile         Filename of the cache file
+     * @param bool                      $forceReload       Whether to invalidate and reload the cache
+     */
+    public function __construct(
+        RouteCollector $routeCollector,
+        callable $dispatcherFactory,
+        string $cacheFile,
+        bool $forceReload = false
+    )
+    {
+        $this->routeCollector    = $routeCollector;
+        $this->dispatcherFactory = $dispatcherFactory;
+        $this->cacheFile         = $cacheFile;
+        $this->forceReload       = $forceReload;
+    }
+
+    /**
+     * Adds a route for a GET request
+     *
+     * @param string $path     The pattern of the path of the route
+     * @param array  $callback The callback of the route
+     *
+     * @return \CodeCollab\Router\Router Return instance of itself to create a fluent interface
+     */
+    public function get(string $path, array $callback): Router
+    {
+        return $this->addRoute('GET', $path, $callback);
+    }
+
+    /**
+     * Adds a route for a POST request
+     *
+     * @param string $path     The pattern of the path of the route
+     * @param array  $callback The callback of the route
+     *
+     * @return \CodeCollab\Router\Router Return instance of itself to create a fluent interface
+     */
+    public function post(string $path, array $callback): Router
+    {
+        return $this->addRoute('POST', $path, $callback);
+    }
+
+    /**
+     * Adds a route
+     *
+     * @param string $verb     The HTTP verb of the route
+     * @param string $path     The pattern of the path of the route
+     * @param array  $callback The callback of the route
+     *
+     * @return \CodeCollab\Router\Router Return instance of itself to create a fluent interface
+     */
+    public function addRoute(string $verb, string $path, array $callback): Router
+    {
+        $this->routeCollector->addRoute($verb, $path, $callback);
+
+        return $this;
+    }
+
+    /**
+     * Gets the dispatcher
+     *
+     * @return \FastRoute\Dispatcher The dispatcher
+     */
+    public function getDispatcher(): Dispatcher
+    {
+        if ($this->forceReload || !file_exists($this->cacheFile)) {
+            $dispatchData = $this->buildCache();
+        } else {
+            $dispatchData = require $this->cacheFile;
+        }
+
+        return call_user_func($this->dispatcherFactory, $dispatchData);
+    }
+
+    /**
+     * Builds the routes cache file
+     *
+     * @return array The dispatch data
+     */
+    private function buildCache(): array
+    {
+        $dispatchData = $this->routeCollector->getData();
+
+        file_put_contents($this->cacheFile, '<?php return ' . var_export($dispatchData, true) . ';');
+
+        return $dispatchData;
+    }
+}
---
+++ test/Unit/RouterTest.php
@@ -0,0 +1,166 @@
+<?php declare(strict_types=1);
+
+namespace CodeCollabTest\Unit\Router;
+
+use CodeCollab\Router\Router;
+
+//use FastRoute\RouteCollector;
+//use FastRoute\Dispatcher;
+
+class RouterTest extends \PHPUnit_Framework_TestCase
+{
+    protected $router;
+
+    public function setUp()
+    {
+        $routeCollector = $this->getMockBuilder('FastRoute\RouteCollector')
+            ->disableOriginalConstructor()
+            ->getMock()
+        ;
+
+        $routeCollector->method('getData')->willReturn(['foo' => 'bar']);
+
+        $this->router = new Router(
+            $routeCollector,
+            function($data) {
+                \PHPUnit_Framework_Assert::assertSame(['foo' => 'bar'], $data);
+
+                return $this->getMock('FastRoute\Dispatcher');
+            },
+            TEST_DATA_DIR . '/cache/routes.php'
+        );
+    }
+
+    public function tearDown()
+    {
+        @unlink(TEST_DATA_DIR . '/cache/routes.php');
+    }
+
+    /**
+     * @covers CodeCollab\Router\Router::__construct
+     * @covers CodeCollab\Router\Router::addRoute
+     * @covers CodeCollab\Router\Router::get
+     */
+    public function testGetReturnsSelf()
+    {
+        $this->assertInstanceOf('CodeCollab\Router\Router', $this->router->get('/', []));
+    }
+
+    /**
+     * @covers CodeCollab\Router\Router::__construct
+     * @covers CodeCollab\Router\Router::addRoute
+     * @covers CodeCollab\Router\Router::post
+     */
+    public function testPostReturnsSelf()
+    {
+        $this->assertInstanceOf('CodeCollab\Router\Router', $this->router->post('/', []));
+    }
+
+    /**
+     * @covers CodeCollab\Router\Router::__construct
+     * @covers CodeCollab\Router\Router::addRoute
+     */
+    public function testAddRouteReturnsSelf()
+    {
+        $this->assertInstanceOf('CodeCollab\Router\Router', $this->router->addRoute('CUSTOMVERB', '/', []));
+    }
+
+    /**
+     * @covers CodeCollab\Router\Router::__construct
+     * @covers CodeCollab\Router\Router::addRoute
+     * @covers CodeCollab\Router\Router::get
+     * @covers CodeCollab\Router\Router::buildCache
+     * @covers CodeCollab\Router\Router::getDispatcher
+     */
+    public function testGetDispatcherNewFile()
+    {
+        $this->assertFalse(file_exists(TEST_DATA_DIR . '/cache/routes.php'));
+
+        $this->assertInstanceOf('FastRoute\Dispatcher', $this->router->get('/', [])->getDispatcher());
+
+        $this->assertTrue(file_exists(TEST_DATA_DIR . '/cache/routes.php'));
+    }
+
+    /**
+     * @covers CodeCollab\Router\Router::__construct
+     * @covers CodeCollab\Router\Router::addRoute
+     * @covers CodeCollab\Router\Router::get
+     * @covers CodeCollab\Router\Router::buildCache
+     * @covers CodeCollab\Router\Router::getDispatcher
+     */
+    public function testGetDispatcherExistingFileDoNotReload()
+    {
+        $routeCollector = $this->getMockBuilder('FastRoute\RouteCollector')
+            ->disableOriginalConstructor()
+            ->getMock()
+        ;
+
+        $routeCollector->method('getData')->willReturn(['foo' => 'bar']);
+
+        $router = new Router(
+            $routeCollector,
+            function($data) {
+                \PHPUnit_Framework_Assert::assertSame(['foo' => 'baz'], $data);
+
+                return $this->getMock('FastRoute\Dispatcher');
+            },
+            TEST_DATA_DIR . '/cache/routes.php'
+        );
+
+        $this->assertFalse(file_exists(TEST_DATA_DIR . '/cache/routes.php'));
+
+        copy(TEST_DATA_DIR . '/routes.php', TEST_DATA_DIR . '/cache/routes.php');
+
+        $this->assertTrue(file_exists(TEST_DATA_DIR . '/cache/routes.php'));
+
+        $this->assertInstanceOf('FastRoute\Dispatcher', $router->get('/', [])->getDispatcher());
+
+        $this->assertTrue(file_exists(TEST_DATA_DIR . '/cache/routes.php'));
+
+        $routes = require TEST_DATA_DIR . '/cache/routes.php';
+
+        $this->assertSame(['foo' => 'baz'], $routes);
+    }
+
+    /**
+     * @covers CodeCollab\Router\Router::__construct
+     * @covers CodeCollab\Router\Router::addRoute
+     * @covers CodeCollab\Router\Router::get
+     * @covers CodeCollab\Router\Router::buildCache
+     * @covers CodeCollab\Router\Router::getDispatcher
+     */
+    public function testGetDispatcherExistingFileDoReload()
+    {
+        $routeCollector = $this->getMockBuilder('FastRoute\RouteCollector')
+            ->disableOriginalConstructor()
+            ->getMock()
+        ;
+
+        $routeCollector->method('getData')->willReturn(['foo' => 'bar']);
+
+        $router = new Router(
+            $routeCollector,
+            function($data) {
+                \PHPUnit_Framework_Assert::assertSame(['foo' => 'bar'], $data);
+
+                return $this->getMock('FastRoute\Dispatcher');
+            },
+            TEST_DATA_DIR . '/cache/routes.php',
+            true
+        );
+
+        $this->assertFalse(file_exists(TEST_DATA_DIR . '/cache/routes.php'));
+
+        copy(TEST_DATA_DIR . '/routes.php', TEST_DATA_DIR . '/cache/routes.php');
+
+        $this->assertTrue(file_exists(TEST_DATA_DIR . '/cache/routes.php'));
+
+        $this->assertInstanceOf('FastRoute\Dispatcher', $router->get('/', [])->getDispatcher());
+
+        $this->assertTrue(file_exists(TEST_DATA_DIR . '/cache/routes.php'));
+
+        $routes = require TEST_DATA_DIR . '/cache/routes.php';
+
+        $this->assertSame(['foo' => 'bar'], $routes);
+    }
+}
